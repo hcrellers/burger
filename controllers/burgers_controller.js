f7e204a4667158f7d0cb5ee0b92577ba4e486186
Halie <halie.creollo@gmail.com>
2019-07-30 23:34:48 -0500

    controllers

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,8 +1,48 @@
-    
+
 const express = require("express");
 const burger = require("../models/burger");
 
+const router = express.Router();
 
+router.get("/", function (req, res) {
+    burger.selectAll(function (data) {
+        var hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+
+});
+
+router.post("/api/burgers", function (req, res) {
+    burger.insertOne("burger_name", req.body.name, function (result) {
+
+        res.json({ id: result.insertId });
+    });
+});
+
+router.put("/api/burgers/:id", function (req, res) {
+    const condition = "id = " + req.params.id;
+
+    console.log("condition", condition);
+
+    burger.updateOne(
+        {
+            devoured: req.body.devoured
+        },
+        condition,
+        function (result) {
+            if (result.changedRows == 0) {
+                // If no rows were changed, then the ID must not exist, so 404
+                return res.status(404).end();
+            }
+            res.status(200).end();
+        }
+
+    );
+
+});
 
 
 
